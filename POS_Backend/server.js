8d27d8884a3b7287db7920e785396824e37879d0
anshu2298 <amshusingh2298@gmail.com>
2025-05-31 09:00:09 +0530

    monor changes

--- POS_Backend/server.js
+++ POS_Backend/server.js
@@ -12,7 +12,8 @@ const app = express();
 
 app.use(
   cors({
-    origin: [process.env.CLIENT_URL, process.env.ADMIN_URL],
+    // origin: [process.env.CLIENT_URL, process.env.ADMIN_URL],
+    origin: "*",
     methods: ["GET", "POST", "PUT", "DELETE", "PATCH"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
