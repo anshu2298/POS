e221382888c34e9e4e29a7e0e3f6c43d5b3a10bc
anshu2298 <amshusingh2298@gmail.com>
2025-05-31 09:28:19 +0530

    minor changes

--- POS_Dashboard/src/utils/apiPaths.js
+++ POS_Dashboard/src/utils/apiPaths.js
@@ -1,23 +1,22 @@
 // const BASE_URL = "http://localhost:3000/api";
-const BASE_URL =
-  "https://forknflamesbackend.vercel.app/api";
+const BASE_URL = "https://forknflamesbackend.vercel.app/";
 export const API_PATHS = {
   TABLES: {
-    GET: `${BASE_URL}/table`,
-    UPDATE: (id) => `${BASE_URL}/table/${id}/reserved`,
-    ADD: `${BASE_URL}/table/add`,
-    DELETE: (id) => `${BASE_URL}/table/${id}`,
+    GET: `${BASE_URL}/api/table`,
+    UPDATE: (id) => `${BASE_URL}/api/table/${id}/reserved`,
+    ADD: `${BASE_URL}/api/table/add`,
+    DELETE: (id) => `${BASE_URL}/api/table/${id}`,
   },
-  MENU: `${BASE_URL}/menu`,
+  MENU: `${BASE_URL}/api/menu`,
   ORDERS: {
-    ADD: `${BASE_URL}/order/create`,
-    GET: `${BASE_URL}/order/get`,
-    GET_PREP_TIME: `${BASE_URL}/order/getPrepTime`,
+    ADD: `${BASE_URL}/api/order/create`,
+    GET: `${BASE_URL}/api/order/get`,
+    GET_PREP_TIME: `${BASE_URL}/api/order/getPrepTime`,
     UPDATE_STATUS: (id) =>
-      `${BASE_URL}/order/${id}/updateStatus`,
+      `${BASE_URL}/api/order/${id}/updateStatus`,
   },
   CUSTOMERS: {
-    GET: `${BASE_URL}/customer/get`,
-    ADD: `${BASE_URL}/customer/add`,
+    GET: `${BASE_URL}/api/customer/get`,
+    ADD: `${BASE_URL}/api/customer/add`,
   },
 };
--- POS_Menu/src/utils/apiPaths.js
+++ POS_Menu/src/utils/apiPaths.js
@@ -1,17 +1,16 @@
 // const BASE_URL = "http://localhost:3000/api";
-const BASE_URL =
-  "https://forknflamesbackend.vercel.app/api";
+const BASE_URL = "https://forknflamesbackend.vercel.app/";
 export const API_PATHS = {
   TABLES: {
-    GET: `${BASE_URL}/table`,
-    UPDATE: (id) => `${BASE_URL}/table/${id}/reserved`,
+    GET: `${BASE_URL}/api/table`,
+    UPDATE: (id) => `${BASE_URL}/api/table/${id}/reserved`,
   },
-  MENU: `${BASE_URL}/menu`,
+  MENU: `${BASE_URL}/api/menu`,
   ORDERS: {
-    ADD: `${BASE_URL}/order/create`,
+    ADD: `${BASE_URL}/api/order/create`,
   },
   CUSTOMERS: {
-    GET: `${BASE_URL}/customer/get`,
-    ADD: `${BASE_URL}/customer/add`,
+    GET: `${BASE_URL}/api/customer/get`,
+    ADD: `${BASE_URL}/api/customer/add`,
   },
 };
