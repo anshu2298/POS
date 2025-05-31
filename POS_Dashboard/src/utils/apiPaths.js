b05321515f89fa86dca621586b4f2fcfd19d5b23
anshu2298 <amshusingh2298@gmail.com>
2025-05-31 08:54:52 +0530

    minor changes

--- POS_Dashboard/src/utils/apiPaths.js
+++ POS_Dashboard/src/utils/apiPaths.js
@@ -1,5 +1,5 @@
-const BASE_URL = "http://localhost:3000/api";
-
+// const BASE_URL = "http://localhost:3000/api";
+const BASE_URL = "https://forknflamesbackend.vercel.app";
 export const API_PATHS = {
   TABLES: {
     GET: `${BASE_URL}/table`,
